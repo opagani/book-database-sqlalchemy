45694943c3c3e67c287e0c301957dc54ee1ec6e2
opagani <paganio@yahoo.com>
2024-06-07 11:32:30 -0700

    First commit

---
+++ app.py
@@ -0,0 +1,8 @@
+# import models
+# main menu add, view, search, analysis, exit
+# function to add books to the database
+# edit books
+# delete books
+# search books
+# data cleaning
+# loop runs program
---
+++ models.py
@@ -0,0 +1,4 @@
+# Create a database
+# Give it a name books.db
+# create a model
+# title, author, Date published, price
